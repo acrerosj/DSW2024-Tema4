1bf4861f80849a4e065653365fdd23fc4ea0a400
Andrés Crespo <acrerosj@canariaseducacion.es>
2024-11-07 16:23:55 +0000

    PDO statement prepare

---
+++ database/proccess_shop.php
@@ -0,0 +1,17 @@
+<?php require 'connectionPDO.php';
+
+if (isset($_POST['username'])){
+  $stmt = $link->prepare("INSERT INTO cart (id, username, amount) VALUES (:id, :username, :amount)");
+  $stmt->bindParam(':id', $clave);
+  $stmt->bindParam(':username', $username);
+  $stmt->bindParam(':amount', $valor);
+
+  $username = $_POST['username'];
+
+  foreach($_POST as $clave => $valor) {
+    if (is_numeric($valor) && $valor > 0) {
+      printf("<p>clave: %s => valor: %s </p>", $clave, $valor);
+      $stmt->execute();
+    }
+  }
+} 
